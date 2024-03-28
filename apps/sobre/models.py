a8116ac0dcba08f444b98555edb077ac6105a05e
Matheus de Lima Bezerra <51747402+MatthLB@users.noreply.github.com>
2024-03-28 17:39:30 -0300

    sobre-backend-Matheus
    sobre-backend-Matheus

---
+++ apps/sobre/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Equipe(models.Model):
+    foto = models.FileField('Foto de Exibição', null=True, blank=True, upload_to='core/static/img/sobre')
+    descricao = models.TextField(verbose_name='Descrição')
+    titulo = models.TextField(verbose_name="Título", max_length=255)
+    nome = models.CharField(verbose_name="Nome", max_length=150)
+
+
+
+    class Meta:
+        verbose_name = "Equipe"
+        db_table = 'equipe'
+
+    def __str__(self):
+        return self.nome
--- setup/settings.py
+++ setup/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
     'curso',
     'aluno',
+    'sobre',
+    'contato',
 ]
 
 MIDDLEWARE = [
