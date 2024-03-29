8f796b995ac24640dc5c7e87a10fa24e94691a2c
Júlio Soares <juliocsoaresa1@gmail.com>
2024-03-29 12:43:42 -0300

    Fix: Ajustes na tela sobre

--- apps/sobre/views.py
+++ apps/sobre/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Equipe
-# Create your views here.
-
 
 def sobre(request):
-    return render(request,'sobre.html', locals())
+    equipe = Equipe.objects.all()
+    return render(request,'sobre/sobre.html', locals())
