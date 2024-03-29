80497ab20ad4a87af3bdc5e41ed670a17d50dcf9
Júlio Soares <juliocsoaresa1@gmail.com>
2024-03-29 19:21:58 -0300

    Fix: Ajuste de configuracao

--- apps/curso/models.py
+++ apps/curso/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Curso(models.Model):
-    foto = models.FileField('Foto de Exibição', null=True, blank=True, upload_to='core/static/img/cursos')
+    foto = models.ImageField('Foto de Exibição', null=True, blank=True, upload_to='core/static/img/cursos')
     nome = models.CharField(verbose_name='Nome', max_length=150)
     area_conhecimento = models.CharField(verbose_name='Área de Conhecimento', max_length=100)
     descricao = models.TextField(verbose_name='Descrição')
--- apps/curso/views.py
+++ apps/curso/views.py
@@ -3,6 +3,7 @@ from .models import Curso
 from aluno.models import Aluno
 
 def index(request):
-    cursos = Curso.objects.order_by('nota')
-    alunos = Aluno.objects.order_by('nome')
+    cursos = Curso.objects.order_by('-nota')
+    alunos = Aluno.objects.order_by('-nome')
+
     return render(request, 'index.html', locals())
