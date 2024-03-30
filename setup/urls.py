715a0975f92133e06da7f47483b354b2feeab076
Matheus de Lima Bezerra <51747402+MatthLB@users.noreply.github.com>
2024-03-29 22:19:33 -0300

    Iniciando contato e testando formulário

--- setup/urls.py
+++ setup/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sobre.urls'))
+    path('', include('sobre.urls')),
+    path('', include('contato.urls'))
 ]
 
