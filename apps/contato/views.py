9b3b08ab8400fa63f0cef8365ce30db344b0adc3
Matheus de Lima Bezerra <51747402+MatthLB@users.noreply.github.com>
2024-04-01 21:25:40 -0300

    feat:formulario-de-contato

--- apps/contato/views.py
+++ apps/contato/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from .models import Contato
 
 def contato(request):
-    novo_contato = Contato()
-    novo_contato.assunto = request.POST.get('assunto')
-    novo_contato.nome = request.POST.get('nome')
-    novo_contato.email = request.POST.get('email')
-    novo_contato.mensagem = request.POST.get('mensagem')
-    novo_contato.save()
+    if request.method == 'POST':
+        novo_contato = Contato()
+        novo_contato.assunto = request.POST.get('assunto')
+        novo_contato.nome = request.POST.get('nome')
+        novo_contato.email = request.POST.get('email')
+        novo_contato.mensagem = request.POST.get('mensagem')
+        novo_contato.save()
     return render(request,'contato/contato.html', locals())
