79ac531f23a140b8e9a61da84d20133e703ab5c4
Júlio Soares <juliocsoaresa1@gmail.com>
2024-04-07 01:10:25 -0300

    Feat: Integracao tela de blog individual

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('<int:noticia_id>', views.blog_post_individual, name='blog_post_individual'),
     path('buscar_noticia/', views.buscar_noticia, name='buscar_noticia'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Noticia
+from django.shortcuts import get_object_or_404
 
 def blog(request):
     noticias = Noticia.objects.order_by('-data_publicacao')
     return render(request, 'blog/blog.html', locals())
+
+def blog_post_individual(request, noticia_id):
+    noticia = get_object_or_404(Noticia, id=noticia_id)
+    return render(request, 'blog/postindividual.html', locals())
 
 def buscar_noticia(request):
     buscar = request.GET.get('buscar', None)
